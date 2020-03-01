09a610e0801f399eba021327eb7686d48ae1c68e
twotl <tobias.awotula@outlook.de>
2020-03-01 16:24:30 +0100

    changed structure

--- javascript.js
+++ javascript.js
@@ -31,9 +31,9 @@ function game() {
     let playerScore = 0;
     let computerScore =0;
     let result;
-
     let roundAmount = prompt('How many rounds do you want to play?:', 5)
     const currentScore = () => `Score:  ${playerScore}:${computerScore}`;
+    
     for (let round = 0; round < roundAmount; round++) {
         playerSelection = prompt(`choose 'Rock', 'Paper' or 'Scissors': `, computerPlay());
         result = playRound(playerSelection, computerPlay());
